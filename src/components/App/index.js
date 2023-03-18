9c61968a80601d190acd9d6cced50155424178dc
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-03-18 23:22:40 +0100

    react configuration

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,9 +5,6 @@ export function App() {
     console.log("Działa w konsoli!")
 
     return (
-        <>
-            <h1>Siema świecie!</h1>
-            <h2>Tip: Check your console</h2>
-        </>
+            <h1>Hello!</h1>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {App} from "./components/App";
+import { App } from "./components/App";
 
 const root = document.getElementById("root");
 
-ReactDOM.render(<App/>, root)
+ReactDOM.render((
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+), root);
