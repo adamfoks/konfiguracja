be562c592e374cea1eee634b9891f4bacea14bb1
adamfoks <100434460+adamfoks@users.noreply.github.com>
2023-03-18 22:26:37 +0100

    react templates files

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+
+export function App() {
+
+    console.log("Działa w konsoli!")
+
+    return (
+        <>
+            <h1>Siema świecie!</h1>
+            <h2>Tip: Check your console</h2>
+        </>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello!");
+import React from "react";
+import ReactDOM from "react-dom";
+import {App} from "./components/App";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App/>, root)
